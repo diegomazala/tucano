15e2bfaa51ec5032e94728c29624ab1d493ea765
Thiago Barroso Perrotta <thiagoperrotta95@gmail.com>
2014-11-07 15:12:23 -0200

    update web documentation

--- misc_8hpp.js
+++ misc_8hpp.js
@@ -1,5 +1,5 @@
 var misc_8hpp =
 [
-    [ "errorCheckFunc", "misc_8hpp.html#a2a283221f1943e4fd0e2949107a0e3f1", null ],
-    [ "initGlew", "misc_8hpp.html#ab843911f1f2bb15fc026ae048c241b95", null ]
+    [ "errorCheckFunc", "misc_8hpp.html#a503213c6201a2f9da5cc5dc63a2e2138", null ],
+    [ "initGlew", "misc_8hpp.html#a44e3b395f453cd7f9da3915d3aeda834", null ]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -4,5 +4,5 @@ var searchData=
   ['enable',['enable',['../classTucano_1_1VertexAttribute.html#ab3122a2169f055f0bd59af9719fbba4e',1,'Tucano::VertexAttribute::enable(GLint loc)'],['../classTucano_1_1VertexAttribute.html#a5ce177db70d867e0a6706a9d55b56c1f',1,'Tucano::VertexAttribute::enable()']]],
   ['endrotation',['endRotation',['../classTucano_1_1Trackball.html#ae68441375b5f42d0285f753c179aa4fd',1,'Tucano::Trackball']]],
   ['endtranslation',['endTranslation',['../classTucano_1_1Trackball.html#a428d57e880d2c86e1b1dc8da5f998ab9',1,'Tucano::Trackball']]],
-  ['errorcheckfunc',['errorCheckFunc',['../namespaceTucano_1_1Misc.html#a2a283221f1943e4fd0e2949107a0e3f1',1,'Tucano::Misc']]]
+  ['errorcheckfunc',['errorCheckFunc',['../namespaceTucano_1_1Misc.html#a503213c6201a2f9da5cc5dc63a2e2138',1,'Tucano::Misc']]]
 ];
