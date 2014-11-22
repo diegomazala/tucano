50d60a2fefa11ee30e516e1d0d724cbc89c5f886
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-11-22 14:13:17 -0200

    add setup-webdoc

---
+++ setup-webdoc.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Setup the documentation folder, so you can run make webdoc later.
+
+confirm() {
+  # call with a prompt string or use a default one
+  read -r -p "${1:-Are you sure? [y/N]} " response
+  case $response in
+    [yY][eE][sS]|[yY]) 
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
+}
+
+msg() {
+  echo "--> $*"
+}
+
+CURRENTDIR="$(pwd)"
+TUCANODIR="$(dirname "$(readlink -f "$0")")"
+WEBDOCDIR="${TUCANODIR}/doc/html"
+
+msg "Removing $WEBDOCDIR"
+confirm "Is this right [y/N]?" && rm -rf "$WEBDOCDIR" || exit 0
+
+msg "Cloning the tucano repository on $WEBDOCDIR"
+git clone https://github.com/LCG-UFRJ/tucano.git "$WEBDOCDIR"
+
+msg "Checking out gh-pages and removing the master branch"
+cd "$WEBDOCDIR" && git checkout gh-pages && git branch -D master && cd "$CURRENTDIR"
+
