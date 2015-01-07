33205c9e909323ff57cce9408354da73c3a233c0
Thiago Perrotta <thiagoperrotta95@gmail.com>
2015-01-07 18:18:03 -0200

    small changes

--- _maintainers/setup-webdoc.sh
+++ _maintainers/setup-webdoc.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 # Automatically setup the documentation folder, so you can run `make webdoc` later.
 
-confirm() {
-  # call with a prompt string or use a default one
-  read -r -p "${1:-Are you sure? [y/N]} " response
-  case $response in
-    [yY][eE][sS]|[yY]) 
-      true
-      ;;
-    *)
-      false
-      ;;
-  esac
-}
-
 msg() {
   echo "--> $*"
 }
@@ -24,7 +11,7 @@ WEBDOCDIR="${TUCANODIR}/doc/html"
 echo $TUCANODIR
 
 msg "Removing $WEBDOCDIR"
-confirm "Is this right [y/N]?" && rm -rf "$WEBDOCDIR" || exit 0
+rm -rf "$WEBDOCDIR"
 
 msg "Cloning the tucano repository on $WEBDOCDIR"
 git clone https://github.com/LCG-UFRJ/tucano.git "$WEBDOCDIR"
