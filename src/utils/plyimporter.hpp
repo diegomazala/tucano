1a023833972d88170d4183920fa1f2d39dc7ba5c
Ricardo <marroquim@cos.ufrj.br>
2015-01-16 11:53:05 -0200

    removed couts outside debug

--- src/utils/plyimporter.hpp
+++ src/utils/plyimporter.hpp
@@ -34,182 +34,182 @@ namespace Tucano
 namespace MeshImporter
 {
 
-static int normal_cb( p_ply_argument argument )
-{
-    static Eigen::Vector3f v;
-
-    void* data;
-    long coord;
-
-    ply_get_argument_user_data( argument, &data, &coord );
-
-    vector<Eigen::Vector3f>* vec = static_cast< vector<Eigen::Vector3f>* >( data );
-
-    switch( coord )
+    static int normal_cb( p_ply_argument argument )
     {
-        case 0:
-        case 1:
-            v[coord] = ply_get_argument_value( argument );
-            break;
+        static Eigen::Vector3f v;
 
-        case 2:
-            v[2] = ply_get_argument_value( argument );
-            vec->push_back( v );
-            break;
+        void* data;
+        long coord;
+
+        ply_get_argument_user_data( argument, &data, &coord );
+
+        vector<Eigen::Vector3f>* vec = static_cast< vector<Eigen::Vector3f>* >( data );
+
+        switch( coord )
+        {
+            case 0:
+            case 1:
+                v[coord] = ply_get_argument_value( argument );
+                break;
+
+            case 2:
+                v[2] = ply_get_argument_value( argument );
+                vec->push_back( v );
+                break;
+        }
+
+        return 1;
     }
 
-    return 1;
-}
-
-static int color_cb( p_ply_argument argument )
-{
-    static Eigen::Vector4f c;
-
-    void* data;
-    long coord;
-
-    ply_get_argument_user_data( argument, &data, &coord );
-
-    float channel = ply_get_argument_value( argument );
-    if (channel > 1.0)
-        channel /= 255.0;
-
-    switch( coord )
+    static int color_cb( p_ply_argument argument )
     {
-        case 0:
-        case 1:
-            c[coord] = channel;
-            break;
+        static Eigen::Vector4f c;
 
-        case 2:
-            c[2] = channel;
-            c[3] = 1.0;
-            (static_cast< vector<Eigen::Vector4f>* >( data ))->push_back( c );
-            break;
+        void* data;
+        long coord;
+
+        ply_get_argument_user_data( argument, &data, &coord );
+
+        float channel = ply_get_argument_value( argument );
+        if (channel > 1.0)
+            channel /= 255.0;
+
+        switch( coord )
+        {
+            case 0:
+            case 1:
+                c[coord] = channel;
+                break;
+
+            case 2:
+                c[2] = channel;
+                c[3] = 1.0;
+                (static_cast< vector<Eigen::Vector4f>* >( data ))->push_back( c );
+                break;
+        }
+
+        return 1;
     }
 
-    return 1;
-}
-
-static int vertex_cb( p_ply_argument argument )
-{
-    static Eigen::Vector4f v;
-    void* data;
-    long coord;
-
-    ply_get_argument_user_data( argument, &data, &coord );
-
-    switch( coord )
+    static int vertex_cb( p_ply_argument argument )
     {
-        case 0:
-        case 1:
-            v[coord] = ply_get_argument_value( argument );
-            break;
+        static Eigen::Vector4f v;
+        void* data;
+        long coord;
 
-        case 2:
-            v[2] = ply_get_argument_value( argument );
-            v[3] = 1.0;
-            (static_cast< vector<Eigen::Vector4f>* >( data ))->push_back( v );
-            break;
+        ply_get_argument_user_data( argument, &data, &coord );
+
+        switch( coord )
+        {
+            case 0:
+            case 1:
+                v[coord] = ply_get_argument_value( argument );
+                break;
+
+            case 2:
+                v[2] = ply_get_argument_value( argument );
+                v[3] = 1.0;
+                (static_cast< vector<Eigen::Vector4f>* >( data ))->push_back( v );
+                break;
+        }
+
+        return 1;
     }
 
-    return 1;
-}
 
-
-static int face_cb( p_ply_argument argument )
-{
-    long value_index;
-    void* data;
-
-    // 3rd parameter is the list length, which is always 3 for triangle meshes
-    // if necessary it can be retrieved passing a reference to a long var
-    ply_get_argument_property( argument, NULL, NULL, &value_index);
-    ply_get_argument_user_data( argument, &data, NULL );
-
-    if (value_index >= 0 && value_index < 3)
+    static int face_cb( p_ply_argument argument )
     {
-        (static_cast< vector<uint>* >(data))->push_back(ply_get_argument_value(argument));
+        long value_index;
+        void* data;
+
+        // 3rd parameter is the list length, which is always 3 for triangle meshes
+        // if necessary it can be retrieved passing a reference to a long var
+        ply_get_argument_property( argument, NULL, NULL, &value_index);
+        ply_get_argument_user_data( argument, &data, NULL );
+
+        if (value_index >= 0 && value_index < 3)
+        {
+            (static_cast< vector<uint>* >(data))->push_back(ply_get_argument_value(argument));
+        }
+
+        return 1;
     }
 
-    return 1;
-}
-
-/**
- * @brief Loads a mesh from an PLY file.
- *
- * @param mesh Pointer to mesh instance to load file.
- * @param filename Given filename of the PLY file.
- */
-static bool loadPlyFile (Mesh *mesh, string filename)
-{
-    p_ply ply = ply_open( filename.c_str(), NULL, 0, NULL );
-    if( !ply || !ply_read_header( ply ) )
+    /**
+     * @brief Loads a mesh from an PLY file.
+     *
+     * @param mesh Pointer to mesh instance to load file.
+     * @param filename Given filename of the PLY file.
+     */
+    static bool loadPlyFile (Mesh *mesh, string filename)
     {
-        cerr << "Cannot open " << filename.c_str() << endl;
-        return false;
+        p_ply ply = ply_open( filename.c_str(), NULL, 0, NULL );
+        if( !ply || !ply_read_header( ply ) )
+        {
+            cerr << "Cannot open " << filename.c_str() << endl;
+            return false;
+        }
+
+        #ifdef TUCANODEBUG
+        cout << "Opening Stanford ply file " << filename.c_str() << endl << endl;
+        #endif
+
+
+        vector<Eigen::Vector4f> vertices;
+        vector<Eigen::Vector3f> norm;
+        vector<Eigen::Vector2f> texCoord;
+        vector<Eigen::Vector4f> color;
+        std::vector<uint> indices;
+
+    //    long nvertices, ncolors, nnormals, ntriangles;
+
+    //    nvertices = ply_set_read_cb( ply, "vertex", "x", vertex_cb, ( void* )&vertices, 0 );
+        ply_set_read_cb( ply, "vertex", "x", vertex_cb, ( void* )&vertices, 0 );
+        ply_set_read_cb( ply, "vertex", "y", vertex_cb, ( void* )&vertices, 1 );
+        ply_set_read_cb( ply, "vertex", "z", vertex_cb, ( void* )&vertices, 2 );
+
+    //    ncolors = ply_set_read_cb( ply, "vertex", "red", vertex_cb, ( void* )&color, 0 );
+        ply_set_read_cb( ply, "vertex", "red", color_cb, ( void* )&color, 0 );
+        ply_set_read_cb( ply, "vertex", "green", color_cb, ( void* )&color, 1 );
+        ply_set_read_cb( ply, "vertex", "blue", color_cb, ( void* )&color, 2 );
+
+    //    nnormals = ply_set_read_cb( ply, "vertex", "nx", normal_cb, ( void* )&norm, 0 );
+        ply_set_read_cb( ply, "vertex", "ny", normal_cb, ( void* )&norm, 1 );
+        ply_set_read_cb( ply, "vertex", "nx", normal_cb, ( void* )&norm, 0 );
+        ply_set_read_cb( ply, "vertex", "nz", normal_cb, ( void* )&norm, 2 );
+
+    //    ntriangles = ply_set_read_cb(ply, "face", "vertex_indices", face_cb, &indices, 0);
+        ply_set_read_cb(ply, "face", "vertex_indices", face_cb, &indices, 0);
+
+        if( !ply_read( ply ) )
+        {
+            return false;
+        }
+
+        ply_close( ply );
+
+        // load attributes found in file
+        if (vertices.size() > 0)
+            mesh->loadVertices(vertices);
+        if (norm.size() > 0)
+            mesh->loadNormals(norm);
+        if (texCoord.size() > 0)
+            mesh->loadTexCoords(texCoord);
+        if (color.size() > 0)
+            mesh->loadColors(color);
+        if (indices.size() > 0)
+            mesh->loadIndices(indices);
+
+        // sets the default locations for accesing attributes in shaders
+        mesh->setDefaultAttribLocations();
+
+
+        #ifdef TUCANODEBUG
+        Misc::errorCheckFunc(__FILE__, __LINE__);
+        #endif
+
+        return true;
     }
-
-    vector<Eigen::Vector4f> vertices;
-    vector<Eigen::Vector3f> norm;
-    vector<Eigen::Vector2f> texCoord;
-    vector<Eigen::Vector4f> color;
-    std::vector<uint> indices;
-
-//    long nvertices, ncolors, nnormals, ntriangles;
-
-//    nvertices = ply_set_read_cb( ply, "vertex", "x", vertex_cb, ( void* )&vertices, 0 );
-    ply_set_read_cb( ply, "vertex", "x", vertex_cb, ( void* )&vertices, 0 );
-    ply_set_read_cb( ply, "vertex", "y", vertex_cb, ( void* )&vertices, 1 );
-    ply_set_read_cb( ply, "vertex", "z", vertex_cb, ( void* )&vertices, 2 );
-
-//    ncolors = ply_set_read_cb( ply, "vertex", "red", vertex_cb, ( void* )&color, 0 );
-    ply_set_read_cb( ply, "vertex", "red", color_cb, ( void* )&color, 0 );
-    ply_set_read_cb( ply, "vertex", "green", color_cb, ( void* )&color, 1 );
-    ply_set_read_cb( ply, "vertex", "blue", color_cb, ( void* )&color, 2 );
-
-//    nnormals = ply_set_read_cb( ply, "vertex", "nx", normal_cb, ( void* )&norm, 0 );
-    ply_set_read_cb( ply, "vertex", "ny", normal_cb, ( void* )&norm, 1 );
-    ply_set_read_cb( ply, "vertex", "nx", normal_cb, ( void* )&norm, 0 );
-    ply_set_read_cb( ply, "vertex", "nz", normal_cb, ( void* )&norm, 2 );
-
-//    ntriangles = ply_set_read_cb(ply, "face", "vertex_indices", face_cb, &indices, 0);
-    ply_set_read_cb(ply, "face", "vertex_indices", face_cb, &indices, 0);
-
-//    cout << nvertices << " " << ncolors << " " << nnormals << " " << ntriangles << endl << endl;
-
-    if( !ply_read( ply ) )
-    {
-        return false;
-    }
-
-    ply_close( ply );
-
-    cout << "vertices : " << vertices.size() << endl;
-    cout << "normals : " << norm.size() << endl;
-    cout << "color : " << color.size() << endl;
-    cout << "texcoords : " << texCoord.size() << endl;
-    cout << "indices : " << indices.size() << endl;
-
-    // load attributes found in file
-    if (vertices.size() > 0)
-        mesh->loadVertices(vertices);
-    if (norm.size() > 0)
-        mesh->loadNormals(norm);
-    if (texCoord.size() > 0)
-        mesh->loadTexCoords(texCoord);
-    if (color.size() > 0)
-        mesh->loadColors(color);
-    if (indices.size() > 0)
-        mesh->loadIndices(indices);
-
-    // sets the default locations for accesing attributes in shaders
-    mesh->setDefaultAttribLocations();
-
-    //errorCheckFunc( __FILE__, __LINE__ );
-
-    return true;
-}
 
 }
 }
