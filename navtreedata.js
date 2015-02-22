8b2f853b92bdb9e526b488717d9a6a252838711b
Thiago Perrotta <thiagoperrotta95@gmail.com>
2015-02-21 22:23:41 -0200

    update web documentation

---
+++ navtreedata.js
@@ -0,0 +1,53 @@
+var NAVTREE =
+[
+  [ "Tucano", "index.html", [
+    [ "Samples", "examples.html", null ],
+    [ "Effects Collection", "effects.html", null ],
+    [ "Using the Effect and Shader Classes", "effect_tutorial.html", null ],
+    [ "Using the Texture Classe", "texture_tutorial.html", null ],
+    [ "Using the Framebuffer Classe", "framebuffer_tutorial.html", null ],
+    [ "Using the Mesh Class", "mesh_tutorial.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", null, [
+      [ "Class List", "annotated.html", "annotated" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classTucano_1_1Framebuffer.html#ae48c5780618d1d3dd31ecb82a55f239f",
+"classTucano_1_1Trackball.html#a7770dc8d59a6c37bf262da118e537bff",
+"rply_8hpp.html#a7b848b72f1f0b43ee543cee3cc07c74aa295a60cb6e03fc3719896d8b9d0827b7"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
